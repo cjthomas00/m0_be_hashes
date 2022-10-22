8a50df701efca2e7deaacd8b10b1fda81ce7ffcc
Caleb Thomas <cjthomas00@comcast.net>
2022-10-22 11:15:43 -0600

    Complete hashes_1.rb

--- hashes_1.rb
+++ hashes_1.rb
@@ -1,30 +1,37 @@
 
-Activity:
+#Activity:
 
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
 # file by entering the following command in your Terminal: 
 # `ruby hashes_1.rb`
 
-foods = {"apples": 23, "grapes": 507, "eggs": 48}
-p zoo
-
-# Write code that prints all of the 'keys' of the foods variable 
-# you created above:
-# YOUR CODE HERE
-
-# Write code that prints all of the 'values' of the foods variable 
-# you created above:
-# YOUR CODE HERE
-
-# Write code that prints the value of the second food of the foods variable 
-# you created above:
-# YOUR CODE HERE
-
-# Write code that adds a food to the foods hash. 
-# Then, print the updated hash:
-# YOUR CODE HERE
-
+foods = {
+  "apples": 23,
+   "grapes": 507,
+    "eggs": 48
+  }
+#
+#
+## Write code that prints all of the 'keys' of the foods variable 
+## you created above: 
+puts foods.keys
+#
+## Write code that prints all of the 'values' of the foods variable 
+## you created above:
+puts foods.values
+#
+## Write code that prints the value of the second food of the foods variable 
+## you created above:
+puts foods["grapes"]
+#
+#
+#
+## Write code that adds a food to the foods hash. 
+## Then, print the updated hash:
+#foods["strawberries"] = true
+#
+puts foods
 
 #-------------------
 # Part 2: Email
@@ -36,17 +43,24 @@ p zoo
 
 # Declare a variable that stores hash. Each key should be an attribute of an email and each
 # value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
+emails = {
+ sender: "Caleb", 
+ receiver: "Mike",
+ subject_line: "Fantasy Football",
+ date: "October 21, 2022",
+ read: true
+}
+## Write code that prints your email hash to the terminal.
+puts emails
+#
+## Write code that prints all of the 'keys' of the email hash 
+## you created above:
+puts emails.keys
+#
+## Write code that prints all of the 'values' of the email hash 
+## you created above:
+puts emails.values
 
-# Write code that prints your email hash to the terminal.
-
-
-# Write code that prints all of the 'keys' of the email hash 
-# you created above:
-# YOUR CODE HERE
-
-# Write code that prints all of the 'values' of the email hash 
-# you created above:
-# YOUR CODE HERE
 
 
 #-------------------
@@ -59,33 +73,33 @@ p zoo
 # Check out the following example of an array of Instagram posts:
 
 
-posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"]
+#posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"]
 
 
 # An Array of Hashes is probably more realistic. Hashes are a data type that's great
 # for storing more complex data. Below, we can store multiple key-value pairs
 # that relate to each individual Instagram post:
-
-
-posts = [
-  {
-    image_src: "./images/beach.png",
-    caption: "At the beach with my besties",
-    timestamp: "4:37 PM August 13, 2019",
-    number_likes: 0,
-    comments: []
-  }, 
-  {
-    image_src: "./images/holiday-party.png",
-    caption: "What a great holiday party omg",
-    timestamp: "11:37 PM December 31, 2019",
-    number_likes: 13,
-    comments: []
-  }
-]
-
-p posts
-p posts[0]
+#
+#
+##posts = [
+#  #{#
+#    #image_src: "./images/beach.png",
+#    #caption: "At the beach with my besties",
+#    ##timestamp: "4:37 PM August 13, 2019",
+#    ##number_likes: 0,
+#    ##comments: []
+#  },## 
+#  {#
+#    i#mage_src: "./images/holiday-party.png",
+#    c#aption: "What a great holiday party omg",
+#    t#imestamp: "11:37 PM December 31, 2019",
+#    n#umber_likes: 13,
+#    c#omments: []
+#  }#
+#]#
+#
+#p posts
+#p posts[0]
 
 
 # The code snippet above shows an Array with 2 elements. Each element in the Array is a 
@@ -97,3 +111,27 @@ p posts[0]
 # YOU DO: Create an array of at least 3 EMAIL Hashes, using the same 
 # key-value pairs you used in your email Hash above.
 # Then, print the email Array to the Terminal.
+emails = [
+  {
+  sender: "Caleb", 
+  receiver: "Mike",
+  subject_line: "Fantasy Football",
+  date: "October 21, 2022",
+  read: true
+}, 
+{ 
+  sender: "Josh",
+  receiver: "Dad",
+  subject_line: "Labor Day trip",
+  date: "August 15, 2022",
+  read: true
+},
+{
+  sender: "Caleb", 
+  receiver: "Joslyn",
+  subject_line: "Dinner Plans",
+  date: "March 3, 2022",
+  read: false
+}
+]
+puts emails
